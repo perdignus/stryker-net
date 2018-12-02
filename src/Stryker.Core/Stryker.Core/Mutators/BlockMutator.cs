8854b6bae5f50056a08f3dd19e507239f680d836
perdignus <gemineavanimeg@gmail.com>
2018-12-02 22:30:16 +0100

    Added support for other block types
    Such as Get properties and Labdas.
    For these types the return type is inferred from its decleration.
    Getter:
    private int GetAndIncrementAmountOfVisitors { get { return _amountOfVisitors+1;}}
    Becomes:
    private int GetAndIncrementAmountOfVisitors { get { return default(int );}}
    
    A labda without parentheses is not recognized, because it has no blocknode
    Labda example:
    Func<int, int> multiplyByThree = (int i) => { return i * 3; };
    Becomes:
    Func<int, int> multiplyByThree = (int i) => { return default(int ); };

--- src/Stryker.Core/Stryker.Core/Mutators/BlockMutator.cs
+++ src/Stryker.Core/Stryker.Core/Mutators/BlockMutator.cs
@@ -4,30 +4,48 @@ using Stryker.Core.Mutants;
 using Microsoft.CodeAnalysis.CSharp;
 using System.Linq;
 using Microsoft.CodeAnalysis;
+using System;
+using Microsoft.Extensions.Logging;
+using Stryker.Core.Logging;
 
 namespace Stryker.Core.Mutators
 {
     public class BlockMutator : Mutator<BlockSyntax>, IMutator
     {
+        private ILogger _logger { get; set; }
+
+        public BlockMutator()
+        {
+            _logger = ApplicationLogging.LoggerFactory.CreateLogger<BlockMutator>();
+        }
+
         public override IEnumerable<Mutation> ApplyMutations(BlockSyntax node)
         {
+            List<Mutation> mutations = new List<Mutation>();
+            try
+            {
+                var blockMutation = CreateBlockMutation(node);
+                if (blockMutation != null)
+                {
+                    mutations.Add(blockMutation);
+                }
+            }
+            catch (ReturnTypeNotFoundException exception)
+            {
+                _logger.LogDebug(exception.Message);
+            }
 
+            return mutations;
+        }
+
+        private Mutation CreateBlockMutation(BlockSyntax node)
+        {
+            Mutation createdBlockMutation = null;
             if (node.Kind() == SyntaxKind.Block)
             {
-                BlockSyntax replacementNode = SyntaxFactory.Block();
-                if (node.Parent.Kind() == SyntaxKind.MethodDeclaration)
-                {
-                    var returnType = GetReturnType(node.Parent);
-                    var isReturnTypeVoid = returnType is PredefinedTypeSyntax predefinedReturnType && predefinedReturnType.Keyword.Text == "void";
-                    if (!isReturnTypeVoid)
-                    {
-                        var defaultReturnType = SyntaxFactory.DefaultExpression(returnType);
-                        var returnStatement = SyntaxFactory.ReturnStatement(defaultReturnType);
-                        replacementNode = SyntaxFactory.Block(returnStatement);
-                    }
-                }
+                BlockSyntax replacementNode = CreateBlockWithCorrectReturnNode(node.Parent);
 
-                yield return new Mutation()
+                createdBlockMutation = new Mutation()
                 {
                     OriginalNode = node,
                     ReplacementNode = replacementNode,
@@ -35,12 +53,85 @@ namespace Stryker.Core.Mutators
                     Type = MutatorType.Block
                 };
             }
+
+            return createdBlockMutation;
+        }
+
+        private BlockSyntax CreateBlockWithCorrectReturnNode(SyntaxNode parentNode)
+        {
+            BlockSyntax replacementNode = SyntaxFactory.Block();
+            var parentType = parentNode.Kind();
+            bool isParentOfTypeMethod = parentType == SyntaxKind.MethodDeclaration;
+            bool isParentOfTypeGetProperty = parentType == SyntaxKind.GetAccessorDeclaration;
+            bool isParentOfTypeLambda = parentType == SyntaxKind.SimpleLambdaExpression || parentType == SyntaxKind.ParenthesizedLambdaExpression;
+
+            if (isParentOfTypeMethod || isParentOfTypeGetProperty || isParentOfTypeLambda)
+            {
+                var nodeWithReturnType = parentNode;
+                if (isParentOfTypeGetProperty)
+                {
+                    nodeWithReturnType = parentNode.Parent.Parent;
+                }
+                else if (isParentOfTypeLambda)
+                {
+                    nodeWithReturnType = GetParentNodeForLambda(parentNode);
+                }
+
+                TypeSyntax returnType = GetReturnType(nodeWithReturnType);
+
+                var isReturnTypeVoid = returnType is PredefinedTypeSyntax predefinedReturnType && predefinedReturnType.Keyword.Text == "void";
+                if (!isReturnTypeVoid)
+                {
+                    var returnStatement = CreateReturnDefaultStatement(returnType);
+                    replacementNode = SyntaxFactory.Block(returnStatement);
+                }
+
+
+            }
+            return replacementNode;
         }
 
         private TypeSyntax GetReturnType(SyntaxNode methodDeclerationNode)
         {
-            var returnType = methodDeclerationNode.ChildNodes().FirstOrDefault(childNode => childNode is TypeSyntax foundReturnType);
+            var returnType = methodDeclerationNode.ChildNodes().LastOrDefault(childNode => childNode is TypeSyntax || childNode is PredefinedTypeSyntax);
             return returnType as TypeSyntax;
+        }
+
+        private SyntaxNode GetParentNodeForLambda(SyntaxNode parentNode)
+        {
+            SyntaxNode nodeWithReturnType;
+            var lambdaDeclerationNode = parentNode.Parent.Parent.Parent;
+            if (lambdaDeclerationNode.Kind() != SyntaxKind.VariableDeclaration)
+            {
+                throw new ReturnTypeNotFoundException("Lambda without inline type declaration not yet supported");
+            }
+
+            var lambdaNodeName = GetFirstChildOfType<GenericNameSyntax>(lambdaDeclerationNode);
+            nodeWithReturnType = GetFirstChildOfType<TypeArgumentListSyntax>(lambdaNodeName);
+
+            return nodeWithReturnType;
+        }
+
+        private SyntaxNode GetFirstChildOfType<T>(SyntaxNode node)
+        {
+            return node.ChildNodes().FirstOrDefault(childNode => childNode is T);
+        }
+
+        private ReturnStatementSyntax CreateReturnDefaultStatement(TypeSyntax returnType)
+        {
+            var defaultReturnType = SyntaxFactory.DefaultExpression(returnType);
+
+            var returnKeyword = SyntaxFactory.Token(SyntaxKind.ReturnKeyword);
+            returnKeyword = returnKeyword.WithTrailingTrivia(SyntaxFactory.Space);
+            var semicolon = SyntaxFactory.Token(SyntaxKind.SemicolonToken);
+
+            var returnStatement = SyntaxFactory.ReturnStatement(returnKeyword, defaultReturnType, semicolon); // Could also be done with just "defaultReturnType", but it would show as "returndefault" instead of "return default"
+            return returnStatement;
+        }
+
+        private class ReturnTypeNotFoundException : NotImplementedException
+        {
+            public ReturnTypeNotFoundException(string message) : base(message) { }
         }
     }
 }
